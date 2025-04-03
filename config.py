186002b64527696800d4241f705d5554d5c58ccf
Pablo Espinosa <sincs87@gmail.com>
2025-04-03 18:35:35 +0200

    Actualizacion

--- config.py
+++ config.py
@@ -21,8 +21,8 @@ class Config:
     MAIL_SERVER = 'smtp.gmail.com'
     MAIL_PORT = 587
     MAIL_USE_TLS = True
-    MAIL_USERNAME = os.environ.get('sincs87@gmail.com')
-    MAIL_PASSWORD = os.environ.get('csmk ogct dqzm qxwe')
-    MAIL_DEFAULT_SENDER = os.environ.get('sincs87@gmail.com')
-    MAIL_DEFAULT_SENDER = os.environ.get("MAIL_DEFAULT_SENDER")
+    MAIL_USE_SSL = False
+    MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
+    MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
+    MAIL_DEFAULT_SENDER = os.environ.get('MAIL_DEFAULT_SENDER')
 
